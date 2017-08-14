b29ea169f7f31e771e1ff599d9fee6a26ffa9601
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-08-14 20:09:02 +0900

    Initial setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  concerning :CommonExceptionHandling do
+    class Forbidden < ActionController::ActionControllerError; end
+    class Locked < ActionController::ActionControllerError; end
+    class InvalidParameter < ActionController::ActionControllerError; end
+    class ServiceUnavailable < ActionController::ActionControllerError; end
+
+    included do
+      rescue_from Exception,                      with: :render_500
+      rescue_from InvalidParameter,               with: :render_400
+      rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionView::MissingTemplate,    with: :render_404
+    end
+
+    def render_500(e = nil)
+      if e
+        logger.error "Rendering 500 with exception: #{e.message}"
+        logger.error "#{e.backtrace[0]}" if e.backtrace.present? and e.backtrace.length > 0
+      end
+      render template: 'errors/error_500', status: 500, content_type: 'text/html'
+    end
+
+    def render_400(e = nil)
+      if e
+        logger.error "Rendering 400 with exception: #{e.message}"
+        logger.error "#{e.backtrace[0]}" if e.backtrace.present? and e.backtrace.length > 0
+      end
+      render template: 'errors/error_400', status: 400, content_type: 'text/html'
+    end
+
+    def render_404(e = nil)
+      if e
+        logger.error "Rendering 404 with exception: #{e.message}"
+        logger.error "#{e.backtrace[0]}" if e.backtrace.present? and e.backtrace.length > 0
+      end
+      render template: 'errors/error_404', status: 404, content_type: 'text/html'
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module SampleUsingZaimApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
