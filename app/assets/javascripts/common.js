64e1f493bc673bb81c572bd4c3ee203f9be9505a
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-08-20 12:10:57 +0900

    Add usage in toppage.

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+  var offsetY = -10;
+  var time = 500;
+
+  $('a[href^="#"]').on('click', function() {
+    var target = $(this.hash);
+    if (!target.length) return ;
+
+    var targetY = target.offset().top + offsetY;
+    $('html, body').animate({scrollTop: targetY}, time, 'swing');
+    window.history.pushState(null, null, this.hash);
+    return false;
+  });
+});
